328b1b4ae5db1b17c8f5483636b50c7345e99c8e
GangHyeokLee <33851859+GangHyeokLee@users.noreply.github.com>
2024-09-14 00:04:34 +0900

    2.4 - Prefetching

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,26 +1,33 @@
 import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import type {AppProps} from "next/app";
 import Link from "next/link";
-import { useRouter } from "next/router";
+import {useRouter} from "next/router";
+import {useEffect} from "react";
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({Component, pageProps}: AppProps) {
 
-  const router = useRouter();
-  const onClickButton = () => {
-    router.push("/text");
-  };
+    const router = useRouter();
 
-  return <>
-    <header>
-      <Link href={"/"} >Index</Link>
-      &nbsp;
-      <Link href={"/search"} >Search</Link>
-      &nbsp;
-      <Link href={"/book/1"} >book/1</Link>
-      <div>
-        <button onClick={onClickButton}>/test 페이지로 이동</button>
-      </div>
-    </header>
-    <Component {...pageProps} />
-  </>;
+    useEffect(() => {
+        router.prefetch('/test');
+    }, []);
+
+
+    const onClickButton = () => {
+        router.push("/text");
+    };
+
+    return <>
+        <header>
+            <Link href={"/"}>Index</Link>
+            &nbsp;
+            <Link href={"/search"} prefetch={false}>Search</Link>
+            &nbsp;
+            <Link href={"/book/1"}>book/1</Link>
+            <div>
+                <button onClick={onClickButton}>/test 페이지로 이동</button>
+            </div>
+        </header>
+        <Component {...pageProps} />
+    </>;
 }
